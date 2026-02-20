dbcf957258e9bc18b33a5c80c43e19e12685fd71
Rojag <szy.pol4@gmail.com>
2026-02-20 16:51:39 +0100

    add the function of compare and save the show command

---
+++ main.py
@@ -0,0 +1,63 @@
+from netmiko import ConnectHandler
+from pathlib import Path
+import difflib
+from datetime import datetime
+
+showCommand = "sh run"
+outputFile = "output.txt"
+compareFile = "compare.txt"
+device = {
+    "device_type": "cisco_xe",
+    "host": "192.168.88.2",
+    "username": "cisco",
+    "password": "cisco123!"
+}
+
+def connectDevice(deviceParameters):
+    connection = ConnectHandler(**deviceParameters) #Create tthe connection,
+#** unpacks the dictionary into keyword arguments:
+# connection = ConnectHandler(device_type="cisco_xe", host="192.168.88.3", username="cisco", password="cisco123!")
+    
+    connection.enable() #priviliged mode 
+    return connection
+
+def saveOutput(PATH, command, device):
+    connection = connectDevice(device)
+    with open(PATH, 'w') as f:
+        f.write(connection.send_command(command))
+
+    connection.disconnect()
+
+
+def compareOutput(PATH, command, device):
+    tempPath = "tempPath.txt"
+    connection = connectDevice(device)
+    with open(tempPath, 'w') as f:
+        f.write(connection.send_command(command))
+    
+    with open(tempPath, 'r') as f1, open(PATH, 'r') as f2:
+        
+       
+
+         line1 = f1.readlines()[6:]
+         line2 = f2.readlines()[6:]
+
+    diff = difflib.unified_diff(
+        line1,
+        line2,
+        fromfile=PATH,
+        tofile=tempPath,
+        lineterm=""
+    )
+    connection.disconnect()
+    Path(tempPath).unlink()
+    return "\n".join(diff)
+
+with open(compareFile, "a") as f:
+    x = compareOutput(outputFile, showCommand, device)
+    if(x != ""):
+        f.write(f"__________________________________________\n{datetime.now()}\n___________________________________________\n")
+        f.write(x)
+        saveOutput(outputFile, showCommand, device)
+    
+
